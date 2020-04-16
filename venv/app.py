cd4ca125982e97c3a3ab5d1f878eb2fadd1d9ebb
Samuel Bridgman <samuel.bridgman@hotmail.co.uk>
2020-04-16 15:45:31 +1200

    First commit

---
+++ venv/app.py
@@ -0,0 +1,170 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash, jsonify, make_response
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+from functools import wraps
+
+#import logging
+#logging.basicConfig(filename='app.log',level=logging.DEBUG)
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisissecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+    #email = db.Column(db.String(80))
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(50))
+    complete = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer)
+
+#@app.route("/", methods=['GET', 'POST'])
+#def home():
+#    name = "sam"
+#    return "<h1>Hello " + name + "</h1>"
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message' : 'Token is missing'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = Users.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
+@app.route("/user", methods=['GET'])
+@token_required
+def get_all_users(current_user):
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot preform that function!'})
+
+    users = Users.query.all()
+
+    output = []
+
+    for user in users:
+        user_data = {}
+        user_data["public_id"] = user.public_id
+        user_data["name"] = user.name
+        user_data["password"] = user.password
+        user_data["admin"] = user.admin
+        output.append(user_data)
+
+    return jsonify({"users" : output})
+
+@app.route('/user/<public_id>',methods=['GET'])
+@token_required
+def get_one_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot preform that function!'})
+
+    user = Users.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message' : 'no user found'})
+
+    user_data = {}
+    user_data["public_id"] = user.public_id
+    user_data["name"] = user.name
+    user_data["password"] = user.password
+    user_data["admin"] = user.admin
+
+    return jsonify({'user' : user_data})
+
+@app.route('/user', methods=['POST'])
+@token_required
+def create_user(current_user):
+    if not current_user.admin:
+        return jsonify({'message': 'Cannot preform that function!'})
+
+    # If content-type application/json not on request force=true required
+#    data = request.get_json(force=true)
+    data = request.get_json()
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    new_user = Users(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message' : 'new user created!'})
+
+@app.route('/user/<public_id>', methods=['PUT'])
+@token_required
+def prompte_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot preform that function!'})
+
+    user = Users.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message' : 'no user found'})
+
+    user.admin = True
+    db.session.commit()
+    return jsonify({'message' : 'User has been promoted!'})
+
+@app.route('/user/<public_id>', methods=['DELETE'])
+@token_required
+def delete_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot preform that function!'})
+
+    user = Users.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message' : 'no user found'})
+
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message' : 'User has been deleted!'})
+
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login Requried!"'})
+
+    user = Users.query.filter_by(name=auth.username).first()
+
+    if not user:
+        return make_response('Could not find user', 401, {'WWW-Authenticate' : 'Basic realm="Login Requried!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id,
+                            'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+                            app.config['SECRET_KEY'])
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could not verify credentials', 401, {'WWW-Authenticate': 'Basic realm="Login Requried!"'})
+
+if __name__ == "__main__":
+        db.create_all()
+        app.run(host='127.0.0.1', port=50000, debug=True)
